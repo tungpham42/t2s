6d03097ef3540899b0e2153635f2f6f6d2ef5c97
Tung Pham <tung.42@gmail.com>
2024-06-28 09:28:30 +0700

    Update getLanguageName.js

--- src/getLanguageName.js
+++ src/getLanguageName.js
@@ -2,10 +2,28 @@ import ISO6391 from "iso-639-1";
 import ISO3166Alpha2 from "iso-3166-1-alpha-2";
 
 const getLanguageName = ({ localeCode }) => {
+  if (!localeCode) {
+    throw new Error("Locale code is required");
+  }
+
   const [languageCode, countryCode] = localeCode.split("-");
+
+  if (!languageCode) {
+    throw new Error(
+      "Invalid locale code format. Expected format: languageCode-countryCode"
+    );
+  }
 
   const languageName = ISO6391.getName(languageCode);
   const countryName = ISO3166Alpha2.getCountry(countryCode);
+
+  if (!languageName) {
+    throw new Error("Invalid language code");
+  }
+
+  if (!countryName) {
+    return languageName;
+  }
 
   return `${languageName} (${countryName})`;
 };
